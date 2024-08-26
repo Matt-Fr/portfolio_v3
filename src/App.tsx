2375e718931001dcd93ff68a721a8d03ca939345
MattFr <matthieu_franck@orange.fr>
2024-08-26 10:59:15 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Presentation from "./sections/Presentation";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+
+function App() {
+  return (
+    <>
+      <Presentation />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/Presentation.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Presentation = (props: Props) => {
+  return <div>Presentation</div>;
+};
+
+export default Presentation;
