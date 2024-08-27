c55cf40c798943e62e4be1ec059b7546d09adb0c
MattFr <matthieu_franck@orange.fr>
2024-08-27 12:01:31 +0200

    add a navbar and a hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
 import Presentation from "./sections/Presentation";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Presentation />
       <Projects />
       <Contact />
--- src/sections/Presentation.tsx
+++ src/sections/Presentation.tsx
@@ -4,19 +4,31 @@ type Props = {};
 
 const Presentation = (props: Props) => {
   return (
-    <section>
-      <div>
-        <h1>Matthieu Franck</h1>
-        <h2>Développeur Front-end</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In magni
-          rerum accusamus nostrum dolore, repellat reprehenderit tempora ex
-          voluptates fugit ipsa voluptas facere ab molestiae! Quisquam eum
-          repellat dignissimos facilis.
-        </p>
-      </div>
-      <div>
-        <div>PICTURE</div>
+    <section id="presentation" className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">Matthieu Franck</h1>
+          <p className="py-6">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+            autem beatae voluptates consequuntur praesentium, sapiente laborum
+            totam vitae cupiditate dolore, maiores veniam iste, modi ipsa quo.
+            Quos tempore ex a repellat debitis doloribus excepturi unde suscipit
+            magnam? Eligendi beatae cumque in maxime quisquam, quas consequatur
+            autem commodi possimus architecto rem maiores excepturi assumenda
+            illum delectus at pariatur saepe placeat, quidem dignissimos impedit
+            fuga, tenetur enim voluptatum! Recusandae possimus laboriosam non
+            laborum facere. Ipsum, exercitationem sapiente voluptatem
+            repellendus voluptatum necessitatibus? Beatae fugit obcaecati
+            consequuntur quos expedita laboriosam magni soluta, nemo quis sint
+            repudiandae atque veniam dignissimos velit quisquam libero
+            perspiciatis harum?
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
       </div>
     </section>
   );
