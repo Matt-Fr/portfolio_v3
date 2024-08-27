4742bea190be8dcb8ed6f46ecc39ef84df4765fe
MattFr <matthieu_franck@orange.fr>
2024-08-27 17:42:41 +0200

    add grid to projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,7 +4,14 @@ import ProjectCard from "../components/ProjectCard";
 
 const Projects = () => {
   return (
-    <section id="projects">
+    <section
+      id="projects"
+      className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center p-4"
+    >
+      <h1 className="text-5xl font-bold col-span-full sm:place-self-start m-5">
+        Mes projets
+      </h1>
+
       {dataProjects.map((project) => (
         <ProjectCard key={project.id} {...project} />
       ))}
