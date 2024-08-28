a003e5fcb742f97e954b1efda1f067ed3e6c3984
MattFr <matthieu_franck@orange.fr>
2024-08-28 17:06:00 +0200

    navbar fix for email

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,27 @@
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 justify-center">
-      <a className="btn btn-ghost text-xl" href="#presentation">
-        Présentation
+    <div className="navbar bg-base-100 justify-between px-4">
+      <a href="mailto:matthieu_franck@orange.fr" className="">
+        <svg
+          className="h-6 w-6 fill-current"
+          viewBox="0 0 512 512"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path>
+        </svg>
       </a>
-      <a className="btn btn-ghost text-xl" href="#projects">
-        Projets
-      </a>
-      <a className="btn btn-ghost text-xl" href="#contact">
-        Contact
-      </a>
+
+      <div>
+        <a className="btn btn-ghost text-xl" href="#presentation">
+          Présentation
+        </a>
+        <a className="btn btn-ghost text-xl" href="#projects">
+          Projets
+        </a>
+        <a className="btn btn-ghost text-xl" href="#contact">
+          Contact
+        </a>
+      </div>
       <label className="swap swap-rotate">
         {/* this hidden checkbox controls the state */}
         <input type="checkbox" className="theme-controller" value="dim" />
