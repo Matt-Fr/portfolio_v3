2f5bbe570ceec727086ebcfdfda4630d96b07f0a
MattFr <matthieu_franck@orange.fr>
2024-08-28 10:46:25 +0200

    fixed issue with id modal

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -13,34 +13,40 @@ type Props = {
 };
 
 const ProjectCard = (props: Props) => {
+  const modalId = `modal_${props.id}`;
+
   return (
     <div className="card glass bg-base-100 w-96 shadow-xl overflow-hidden">
       <div className="h-60 flex justify-center items-start overflow-hidden ">
-        <img src={props.picture} alt="car!" className="object-cover" />
+        <img
+          src={props.picture}
+          alt={`${props.title} image`}
+          className="object-cover"
+        />
       </div>
       <div className="card-body">
         <h2 className="card-title">{props.title}</h2>
-        <p>How to park your car at your garage?</p>
+        <p>{props.descriptionEn.substring(0, 100)}...</p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Learn now!</button>
           <button
             className="btn"
             onClick={() => {
-              const modal = document.getElementById("my_modal_2");
+              const modal = document.getElementById(modalId);
               if (modal) {
                 (modal as HTMLDialogElement).showModal();
               }
             }}
           >
-            open modal
+            Open Modal
           </button>
-          <dialog id="my_modal_2" className="modal">
+          <dialog id={modalId} className="modal">
             <div className="modal-box">
               <h3 className="font-bold text-lg">{props.title}</h3>
               <p className="py-4">{props.descriptionFr}</p>
             </div>
             <form method="dialog" className="modal-backdrop">
-              <button>close</button>
+              <button>Close</button>
             </form>
           </dialog>
         </div>
