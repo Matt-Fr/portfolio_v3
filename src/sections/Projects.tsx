4c9263598fcc0dd8c30da470508abe093a07307e
MattFr <matthieu_franck@orange.fr>
2024-08-29 18:12:06 +0200

    fixed projects responsive

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <section
       id="projects"
-      className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center p-4"
+      className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center "
     >
       <h1 className="text-5xl font-bold col-span-full sm:place-self-start m-5">
         Mes projets
